535da63b8b959549006ae5ca1e98438888469794
Nischal Samji <nischalsamji@gmail.com>
2016-05-03 23:59:07 -0700

    Remove bugs

--- wordgen.py
+++ wordgen.py
@@ -2,11 +2,16 @@ worddict = {}
 alphabetmap = {}
 
 def genwords(length):
-
+    pass
 def loadwords():
     wordfile = open("words.txt")
     words = wordfile.readlines()
     for word in words:
         wordlength = len(word)
-        if worddict.haskey(wordlength):
-            wordict[wordlength][] = word
+        if worddict.has_key(wordlength):
+            worddict[wordlength][word[0]] = word
+        else:
+            worddict[wordlength] = word
+
+if __name__ == '__main__':
+    loadwords()
