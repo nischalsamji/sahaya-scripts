7300360ea4ffe9ea18d3a524b9cba273076c1349
Nischal Samji <nischalsamji@gmail.com>
2016-05-05 23:59:17 -0700

    Almost Complete

--- wordgen.py
+++ wordgen.py
@@ -4,14 +4,13 @@ alphabetmap = {}
 def genwords(length):
     pass
 def loadwords():
+    letters = 'abcdefghijklmnopqrstuvwxyz'
     wordfile = open("words.txt")
     words = wordfile.readlines()
-    for word in words:
-        wordlength = len(word)
-        if worddict.has_key(wordlength):
-            worddict[wordlength][word[0]] = word
-        else:
-            worddict[wordlength] = word
-
+    for letter in range(len(letters)):
+        letterwords = [word.rstrip('\n') for word in words if word[0] == letters[0] and len(word) == 4]
+        alphabetmap['a'] = letterwords
+        worddict[3] = alphabetmap
+    print worddict
 if __name__ == '__main__':
     loadwords()
