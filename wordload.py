04440dbfc08eb0717fc11b5161163e82c7a3bf29
Nischal Samji <nischalsamji@gmail.com>
2016-05-07 22:35:26 -0700

    Add Interactivity for printing

--- wordload.py
+++ wordload.py
@@ -1,4 +1,12 @@
 import cPickle as pickle
+words = {}
 
-words = pickle.load(open( "worddict.pkl", "rb" ))
-print words[7]['z']
+def printwords(length, letter):
+	print words[int(length)][letter]
+	
+
+if __name__ == '__main__':
+	words = pickle.load(open( "worddict.pkl", "rb" ))
+	length = raw_input('Enter the length of the words\n')
+	letter = raw_input('Enter a character\n')
+	printwords(length, letter)
